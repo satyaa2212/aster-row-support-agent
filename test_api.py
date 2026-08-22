a0403e329e0ab2cf480f993e6a7676d9c4fc3e34
satyaa2212 <147073681+satyaa2212@users.noreply.github.com>
2026-08-23 02:26:12 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 load_dotenv()
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-# List se latest flash model use kar rahe hain
+# From list I'm taking latest gemini-3.7-flash Model.
 model = genai.GenerativeModel('models/gemini-3.7-flash')
 
 response = model.generate_content("Say hello in one word")
